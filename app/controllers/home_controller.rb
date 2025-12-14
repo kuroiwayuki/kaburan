76a2bd3091832a15e62ef1b5883db848114f2d35
kuroiwayuki <yuki8050@icloud.com>
2025-12-14 21:13:23 +0900

    ランディングページの追加と参加コード表示機能の実装

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+class HomeController < ApplicationController
+  def index
+    # ログイン済みの場合は買い物メモ一覧にリダイレクト
+    redirect_to memos_path if user_signed_in?
+  end
+end
+

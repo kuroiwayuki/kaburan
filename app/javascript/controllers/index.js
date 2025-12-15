5ac2a0bd46641c2167a57e72bcfd743b70466b9b
kuroiwayuki <yuki8050@icloud.com>
2025-12-15 12:30:59 +0900

    fix: Remove non-existent @hotwired/stimulus-loading package

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -1,6 +1,4 @@
-import { application } from "controllers/application"
-
-// Eager load all controllers defined in the import map under controllers/**/*_controller
-import { eagerLoadControllersFrom } from "@hotwired/stimulus-loading"
-eagerLoadControllersFrom("controllers", application)
+// Stimulus controllers will be loaded here when needed
+// For now, just export the application
+export { application } from "./application"
 
